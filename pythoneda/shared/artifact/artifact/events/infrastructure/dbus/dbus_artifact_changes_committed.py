f56c8b1451d94c5e90b42d035577ef984038466c
rydnr <github@acm-sl.org>
2024-12-23 20:42:27 +0100

    Refactored d-bus events

--- pythoneda/shared/artifact/artifact/events/infrastructure/dbus/dbus_artifact_changes_committed.py
+++ pythoneda/shared/artifact/artifact/events/infrastructure/dbus/dbus_artifact_changes_committed.py
@@ -20,16 +20,17 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 from dbus_next import Message
-from dbus_next.service import ServiceInterface, signal
+from dbus_next.service import signal
 import json
-from pythoneda.shared import BaseObject
+from pythoneda.shared import Event
+from pythoneda.shared.infrastructure.dbus import DbusEvent
 from pythoneda.shared.artifact.events import Change
 from pythoneda.shared.artifact.artifact.events import ArtifactChangesCommitted
 from pythoneda.shared.artifact.artifact.events.infrastructure.dbus import DBUS_PATH
-from typing import List
+from typing import List, Type
 
 
-class DbusArtifactChangesCommitted(BaseObject, ServiceInterface):
+class DbusArtifactChangesCommitted(DbusEvent):
     """
     D-Bus interface for ArtifactChangesCommitted
 
@@ -113,6 +114,15 @@ class DbusArtifactChangesCommitted(BaseObject, ServiceInterface):
             json.loads(prev_event_ids),
             event_id,
         )
+
+    @classmethod
+    def event_class(cls) -> Type[Event]:
+        """
+        Retrieves the specific event class.
+        :return: Such class.
+        :rtype: type(pythoneda.shared.Event)
+        """
+        return ArtifactChangesCommitted
 
 
 # vim: syntax=python ts=4 sw=4 sts=4 tw=79 sr et
